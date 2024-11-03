d0b14ed42ef650fc7ba003fb43c5da7c1c537b0e
nmset0 <nmset0@colostate.edu>
2024-11-02 18:11:02 -0600

    File for plotting data

---
+++ overwatch_plotting.R
@@ -0,0 +1,9 @@
+# Packages:
+library(ggplot2)
+library(readr)
+library(dplyr)
+
+OverwatchHeroData_clean = read_csv("Datasets/OverwatchHeroData_clean.csv")
+role_buff_nerf_ratios = read_csv("Datasets/role_buff_nerf_ratios.csv")
+hero_buff_nerf_ratios = read_csv("Datasets/hero_buff_nerf_ratios.csv")
+skin_ratios = read_csv("Datasets/skin_ratios.csv")
